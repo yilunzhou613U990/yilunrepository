bc2eed5acf2bcb60d2dd64bc06f2fc8ee3d3bda7
“zmdbzaaa” <1684862318@qq.com>
2022-07-13 18:58:07 +0800

    “第一次提交”

---
+++ code/chapter2/src/main/java/com/smart/dao/UserDao.java
@@ -0,0 +1,20 @@
+package com.smart.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public int getMatchCount(String userName, String password) {
+        Integer integer = jdbcTemplate.queryForObject(" SELECT count(*) FROM t_user where user_name=? and password=?", new Object[]{userName, password}, Integer.class);
+        return 0;
+    }
+}
